c9862edcb204aff40cacb4a388174761eabebf02
Sergiu Rosca <rmagnum2002@gmail.com>
2015-01-14 17:59:17 +0200

    init for applets

---
+++ src/init_for_applets/Main.java
@@ -0,0 +1,17 @@
+package init_for_applets;
+import javax.swing.JApplet;
+
+public class Main extends JApplet {
+
+	private double sum;
+	
+	public void init(){
+//		https://www.youtube.com/watch?v=Ud9G_AFqTsk
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
---
+++ src/put_applet_in_website/Main.java
@@ -0,0 +1,10 @@
+package put_applet_in_website;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JApplet {
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawString("wow, this worked", 25, 25);
+	}
+}
