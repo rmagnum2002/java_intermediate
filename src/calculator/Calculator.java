ded9810918a6492b8516b92aeec18db2bd1a73d2
Sergiu Rosca <rmagnum2002@gmail.com>
2015-01-21 18:20:44 +0200

    building calc

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -13,14 +13,21 @@ public class Calculator extends JFrame {
 	private JPanel buttons;
 	private JPanel nbuttons;
 	private JPanel fbuttons;
+	private double a1;
+	private double a2;
 	
 	public Calculator(){
 		super("Calculator");
+		screenBar = new JTextField();
+		screenBar.setText("0");
+		screenBar.setHorizontalAlignment(JTextField.RIGHT);
+		screenBar.setFont(new java.awt.Font("Arial", Font.BOLD, 36));
+		screenBar.setEditable(false);
 		
 		JButton[] fButtons = new JButton[7];
 		JButton[] numberButtons = new JButton[10];
 		
-		String[] arr = {"cButton", "multiplyButton", "multiplyButton", "addButton", "substractButton", "enterButton"};
+		String[] arr = {"cButton", "multiplyButton", "divideButton", "addButton", "substractButton", "enterButton"};
 		String[] arr1 = {"C", "*", "/", "+", "-", "Enter"};
 		
 		for (int i = 0; i < arr.length; i++) {
@@ -30,12 +37,6 @@ public class Calculator extends JFrame {
 		for (int i = 9; i >= 0; i--) {
 			numberButtons[i] = new JButton(Integer.toString(i));
 		}
-		
-		screenBar = new JTextField();
-		screenBar.setText("0");
-		screenBar.setHorizontalAlignment(JTextField.RIGHT);
-		screenBar.setFont(new java.awt.Font("Arial", Font.BOLD, 36));
-		screenBar.setEditable(false);
 		
 		buttons = new JPanel();
 		buttons.setBackground(Color.BLUE);
@@ -48,10 +49,16 @@ public class Calculator extends JFrame {
 
 			numberButtons[i].addActionListener(
 				new ActionListener() {
-				
+					
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						screenBar.setText("7");
+						if (screenBar.getText().equals("0")){
+							screenBar.setText("7");
+						}else{
+							if (screenBar.getText().length() <= 10) {
+								screenBar.setText(screenBar.getText().concat("6"));
+							}
+						}
 					}
 				}
 			);
@@ -69,7 +76,8 @@ public class Calculator extends JFrame {
 					
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						System.out.println("f button was clicked");						
+						System.out.println("f button was clicked");
+						System.out.println(((JButton) e.getSource()).getName() + " Click");
 					}
 				}
 			);
